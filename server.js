818029e7a7777761defb62ff364f1eb82cbfaf02
Orange <42256381+0-a-e@users.noreply.github.com>
2021-06-24 11:07:25 +0900

    一回だけ取得可能

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-var express = require("express");
+const express = require("express");
 const { Mtoken } = require("./Mtoken.js");
-var app = express();
+const request = require('request');
+
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
+    //変えれるようにしたほうがいい？いやMtoken固定だから無理か
     console.log(Mtoken);
-    res.status(200).send("Hello World");
+    request.post({
+        url: 'https://msk.seppuku.club/api/admin/emoji/list',
+        headers: {
+          "content-type": "application/json"
+        },
+        body: JSON.stringify({"i":Mtoken,"limit":100})
+    },
+     function (error, response, body){
+         const data = JSON.parse(body);
+        console.log(data[data.length - 1]["id"]);
+        res.status(200).send(JSON.stringify(body));
+      });
+  //  res.status(200).send("Hello World");
 });
 
 app.listen(port); // appを特定のportでlistenさせる。
