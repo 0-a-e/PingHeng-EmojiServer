2e3b1b9002fc8c6ce114021de119e5a3c83ae057
Orange <42256381+0-a-e@users.noreply.github.com>
2021-06-25 08:02:11 +0900

    一応できた　199回ま

--- server.js
+++ server.js
@@ -1,26 +1,76 @@
 const express = require("express");
 const { Mtoken } = require("./Mtoken.js");
-const request = require('request');
-
+const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    //変えれるようにしたほうがいい？いやMtoken固定だから無理か
-    console.log(Mtoken);
-    request.post({
-        url: 'https://msk.seppuku.club/api/admin/emoji/list',
-        headers: {
-          "content-type": "application/json"
-        },
-        body: JSON.stringify({"i":Mtoken,"limit":100})
-    },
-     function (error, response, body){
-         const data = JSON.parse(body);
-        console.log(data[data.length - 1]["id"]);
-        res.status(200).send(JSON.stringify(body));
-      });
-  //  res.status(200).send("Hello World");
+const port = process.env.PORT || 3000;
+let aldata = [];
+let sinceId = "";
+
+const getbodydata = (iffirst,id) => {
+    let bodydata = "";
+
+    if(iffirst){
+        bodydata = JSON.stringify({
+            "i":Mtoken,
+            "limit":100,
+        })
+
+    } else {
+        bodydata = JSON.stringify({
+            "i":Mtoken,
+            "limit":100,
+            "sinceId":id
+        })
+    }
+    return bodydata;
+
+}
+
+const gett = async function(bodydata){
+    try{
+        console.log(bodydata);
+        //変えれるようにしたほうがいい？いやMtoken固定だから無理か
+        const res = await axios.post('https://msk.seppuku.club/api/admin/emoji/list',bodydata, {
+            headers: {'Content-Type': 'application/json'}
+          });
+
+        if (res.status != 200) {
+            console.log("例外発生時の処理")
+         }
+
+            const data = res.data;
+            console.log(aldata.length.toString() + " 合計//今回 " + data.length.toString());
+
+            if (data.length != 0) {
+                const d = aldata.concat(data);
+                aldata = d;
+                const lastid = data[data.length - 1]["id"];
+                console.log(lastid);
+                return lastid;
+            } else {
+                console.log("==end==");
+                return false;
+            }
+    
+    } catch(err) {
+    //    console.log(err);
+    }
+}
+
+app.get("/", async(req, res) => {
+    sinceId = await gett(getbodydata(true,false));
+    while(true){
+        const r = await gett(getbodydata(false,sinceId));
+        if(r){
+            sinceId = r;
+        } else {
+            res.status(200).send(aldata);
+            break;
+        }
+    }
+    aldata = [];
+    sinceId = "";
 });
 
 app.listen(port); // appを特定のportでlistenさせる。
